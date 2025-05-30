ba668715f83f5e94d3947bc637802938f903b8ff
Joao Vitor Caetano <caetanojv99@gmail.com>
2025-05-30 14:08:24 -0300

    Adicionando imagem da logo Biocamp

--- chamados_ti/chamados_app.py
+++ chamados_ti/chamados_app.py
@@ -32,7 +32,7 @@ def show_logo(logo_path, width=200):
     )
 
 # ====== EXIBIR O LOGO ======
-show_logo("chamados_ti/LOGO BIOCAMP PB.jpg")
+show_logo("chamados_ti/Llogobiobranco.png.jpg")
 
 # ====== CONFIGURAÇÕES DO E-MAIL ======
 EMAIL_REMETENTE = "tecsuporteti@biocamp.com.br"
